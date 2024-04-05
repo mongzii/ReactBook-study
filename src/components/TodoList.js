ebeea4ef097aadbcdf596ee7c9c2f961ecacc077
moonjiyoung <umarg228@naver.com>
2024-04-05 17:04:03 +0900

    feat: study중

---
+++ src/components/TodoList.js
@@ -0,0 +1,14 @@
+import TodoListItem from "./TodoListItem";
+import "./TodoList.css";
+
+const TodoList = () => {
+  return (
+    <div className="TodoList">
+      <TodoListItem />
+      <TodoListItem />
+      <TodoListItem />
+    </div>
+  );
+};
+
+export default TodoList;
